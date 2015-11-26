b2c2145635ca01d06e0c73b4016769211246bcb9
markusFrost <andreywystawkin@yandex.ru>
2015-11-26 10:41:07 +0300

    Initial Commit

---
+++ SportsStore/SportsStore.WebUI/Models/PagingInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.WebUI.Models
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; } // сколько всего элементов в БД
+        public int ItemsPerPage { get; set; } // сколько элементо вдолжно быть на странице
+        public int CurrentPage { get; set; } // текущая страница
+        public int TotalPages// сколько всего будет сtраниц во View
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage); }
+        }
+    }
+
+}
